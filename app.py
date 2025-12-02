202f3a020f1ec0679ee1c4b5754639f94e48659d
Ritik kumar gupta <rg4092739@gmail.com>
2025-12-02 18:52:50 +0530

    Fix

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
         "https://*.netlify.app",
-        "https://*.onrender.com"
+        "https://*.onrender.com",
+        "https://*.zeabur.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
